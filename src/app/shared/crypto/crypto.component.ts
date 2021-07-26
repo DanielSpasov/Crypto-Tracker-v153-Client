215f52abb5d0b04c5c561a7eb931f3eb3e2c3ac4
Daniel Spasov <danielspasov4@gmail.com>
2021-07-26 09:17:35 +0300

    Currencies at the top 100 page can now be sorted.
    Sorting works for:
    
    #Rank (ascending & descending)
    
    #Name (ascending & descending)
    
    #Price (ascending & descending)
    
    #Percent Change the last 24h (ascending & descending)
    
    #Percent Change the last 7d (ascending & descending)
    
    #Market Cap (ascending & descending)

--- src/app/shared/crypto/crypto.component.ts
+++ src/app/shared/crypto/crypto.component.ts
@@ -14,14 +14,41 @@ import { ICrypto } from '../../interfaces/crypto.model';
 export class CryptoComponent implements OnInit {
 
     cryptos!: ICrypto[];
+    sorting!: { [key: string]: boolean };
 
     constructor(private cryptoService: CryptoService) { }
 
-    loadItems(): void { this.cryptoService.getTop100().subscribe(res => this.cryptos = res.data) }
-    reloadItems(): void { this.cryptos = []; this.loadItems() }
-    ngOnInit(): void { this.loadItems() }
+    loadItems(): void {
+        this.cryptoService
+            .getTop100()
+            .subscribe(res => this.cryptos = res.data)
+    }
+    ngOnInit(): void {
+        this.loadItems()
+        this.sorting = {
+            cmc_rank: false,
+            name: false,
+            price: false,
+            percent_change_24h: false,
+            percent_change_7d: false,
+            market_cap: false,
+        }
+    }
 
     addToWatchlist(crypto: string): void { this.cryptoService.addToWatchlist(crypto).subscribe(data => console.log(data)) }
     removeFromWatchlist(crypto: string): void { this.cryptoService.removeFromWatchlist(crypto).subscribe(data => console.log(data)) }
 
+    sortItems(criteria: string): void {
+        if (criteria === 'name') {
+            if (this.sorting[criteria]) this.cryptos.sort((a, b) => b.name.localeCompare(a.name));
+            if (!this.sorting[criteria]) this.cryptos.sort((a, b) => a.name.localeCompare(b.name));
+        } else if (criteria === 'cmc_rank') {
+            if (this.sorting[criteria]) this.cryptos.sort((a, b) => b.cmc_rank - a.cmc_rank);
+            if (!this.sorting[criteria]) this.cryptos.sort((a, b) => a.cmc_rank - b.cmc_rank);
+        } else {
+            if (this.sorting[criteria]) this.cryptos.sort((a, b) => a.quote.USD[criteria] - b.quote.USD[criteria]);
+            if (!this.sorting[criteria]) this.cryptos.sort((a, b) => b.quote.USD[criteria] - a.quote.USD[criteria]);
+        }
+        this.sorting[criteria] = !this.sorting[criteria]
+    }
 }
