aafe59befa1e2d08df68dd4c2f84043555471e23
Daniel Spasov <danielspasov4@gmail.com>
2021-08-01 08:03:49 +0300

    Added sign in.

--- src/app/shared/sign-in/sign-in.component.ts
+++ src/app/shared/sign-in/sign-in.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+
+
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+    selector: 'app-sign-in',
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private userService: UserService,
+        private router: Router
+    ) { }
 
+    ngOnInit(): void {
+        this.userService.checkLoggedIn()
+    }
+
+    signInSubmit(form: NgForm): void {
+        if (form.invalid) return;
+
+        let userData = {
+            email: form.controls.email.value,
+            password: form.controls.password.value
+        };
+
+        this.userService
+            .signIn(userData)
+            .subscribe(
+                data => {
+                    let token = data.token;
+                    localStorage.setItem('auth-token', token)
+                    this.router.navigate(['/']);
+                },
+                err => console.log(err.error.message)
+            )
+    }
 }
--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -12,6 +12,32 @@ export class UserService {
     getUser() { return this.http.get<IUser>('http://localhost:4153/user/getOne'); }
     getUsers() { return this.http.get<Array<IUser>>('http://localhost:4153/user/getAll'); }
 
-    signUp(userData: object) { return this.http.post<IUser>('http://localhost:4153/user/sign-up', userData) }
+    signUp(userData: object) { return this.http.post<any>('http://localhost:4153/user/sign-up', userData) }
+    signIn(userData: object) { return this.http.post<any>('http://localhost:4153/user/sign-in', userData) }
 
+    checkLoggedIn() {
+        try {
+            let token = localStorage.getItem('auth-token')
+            if (token === null) {
+                localStorage.setItem('-authtoken', '')
+                token = ''
+            }
+
+            this.http.post<any>(`http://localhost:4153/user/tokenIsValid`, null, {
+                headers: { 'x-auth-token': token }
+            }).subscribe(
+                data => {
+                    if (data.isAuth) {
+                        this.http.get(`http://localhost:4153/user/${data._id}`, {
+                            headers: { 'x-auth-token': data.token }
+                        }).subscribe(
+                            data => console.log(data),
+                            err => console.log(err)
+                        )
+                    }
+                },
+                err => console.log(err)
+            )
+        } catch (err) { console.log(err.error.message) }
+    }
 }
