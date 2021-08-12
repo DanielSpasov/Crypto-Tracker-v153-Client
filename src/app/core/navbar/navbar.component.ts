cbf0ed9c319b56bba6c8127dcfb2fa095232ad9a
Daniel Spasov <danielspasov4@gmail.com>
2021-08-12 09:38:15 +0300

    In navbar it now says who are you logged in as.

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { IUser } from 'src/app/interfaces/user.model';
 
 import { AuthService } from '../../auth/auth.service';
+import { UserService } from 'src/app/user/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 
 
@@ -9,11 +13,28 @@ import { AuthService } from '../../auth/auth.service';
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
     isAuth = this.authService.isAuth();
-    userID = localStorage.getItem('user-id')
+    user!: IUser;
 
-    constructor(private authService: AuthService) { }
+    constructor(
+        private authService: AuthService,
+        private userService: UserService,
+        private toastr: ToastrService
+    ) { }
+
+    ngOnInit(): void {
+
+        let userID = localStorage.getItem('user-id');
+        if(!userID) return;
+
+        this.userService
+            .getUser(userID)
+            .subscribe(
+                data => this.user = data,
+                err => this.toastr.error(err.error.message)
+            );
+    };
 
 }
