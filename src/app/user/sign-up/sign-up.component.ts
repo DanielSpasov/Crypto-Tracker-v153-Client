0a81274d6376d339643b2258f0fa1a43d88674f0
Daniel Spasov <danielspasov4@gmail.com>
2021-08-14 11:34:51 +0300

    Made sign up validation better.

--- src/app/user/sign-up/sign-up.component.ts
+++ src/app/user/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { ToastrService } from 'ngx-toastr';
 import { UserService } from '../user.service';
 
 
@@ -12,10 +13,22 @@ import { UserService } from '../user.service';
 })
 export class SignUpComponent {
 
-    constructor(private userService: UserService) { }
+    constructor(
+        private userService: UserService,
+        private toastr: ToastrService
+    ) { }
 
     signUpSubmit(form: NgForm): void {
-        if (form.invalid) return;
+        if (form.invalid) {
+            if (form.controls.email.errors?.required) this.toastr.error('Email is required');
+            if (form.controls.email.errors?.isEmail) this.toastr.error('Email is Invalid');
+            if (form.controls.password.errors?.required) this.toastr.error('Password is required');
+            if (form.controls.password.errors?.minlength) this.toastr.error('Password must be at least 6 symbols long');
+            if (form.controls.rePassword.errors?.required) this.toastr.error('Repeat Password is required');
+            if (form.controls.rePassword.errors?.minlength) this.toastr.error('Repeat Password must be at least 6 symbols long');
+            if (form.controls.rePassword.errors?.sameValue) this.toastr.error('Passwords doesn\'t match');
+            return;
+        }
 
         let userData = {
             email: form.controls.email.value,
